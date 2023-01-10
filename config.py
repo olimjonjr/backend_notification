03a84a0f1208034d11164a1e2ba3eb0adea0e8da
Olimjon <doniyorjon15@gmail.com>
2023-01-10 12:50:47 +0500

    updated views.py

--- config.py
+++ config.py
@@ -2,6 +2,15 @@ from firebase_admin import credentials
 
 
 cred = credentials.Certificate({
-  # your firebase secret key
+  "type": "service_account",
+  "project_id": "djangofcm-ffc7c",
+  "private_key_id": "5940cccdfe0dc1db17c9da182b902c09204e5726",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDP0ufc23B54CuG\neVbSLCS45yN6kwA26sfXcNix5z2nhtCmIqQZPmdOV4haVWAjAbo3+Fg4vS0TiAcw\nfpbf7zv09H1UPV9vt9PS6V25aTspP+3xTjlBAs7Wfmhjoi30J/KXNAAzqZp0PGCu\nV5oDpURa/cKvWwG9YYboVX/CU9Xp2RIaOUqXh+zTCRx+DhmEbL+bCUVM4CfObPp1\nlfTWju+ec9ZPyDToPq5uBeuJNIPyPWk9Pj2b4523fw66nyWwu212R49/SbuuOyVn\nMnkF8BKWJN3UTLvD88C+WEG9v7xfQIbLslVchFdpQkzEF4i9WWsvC05Opma4ZUiT\n8SsmjL/3AgMBAAECggEACWEIS8RaKss+IPSAQZNHw6coHROzR8cyu6YBHDjxDwrG\nMdPluf9B+ZwjsDKi2GbvONrBXdYpHvTdoNz7AXeHf4I6gb8UpohnoTYc9Q2AY60v\nQjD4joGKRM3cTNrN3h32FHLUANY0/UKYTmXvsnMWnFClekp4z/YtziBwV/HL+UtY\nsItqJQQyW3JEnPYUdt5vMXAFrmeKMljYje8mD2oux2tDyhD8tsd3BWEAxYO8bU9T\nAIXlQyV/Piuw9rmccQGyNxYKb+8z8vUhvp6DlMnEfulPB8Y+i7bPYjzK7Za1LSyP\nFSQ96pjE+km13wrIw3FX4SrCofc2G3ScvtvSpcgBGQKBgQD+MHz1KYufo26HtbRH\nhX+v8gSGNpKEc/TGDnKtJ8EW2tZzDp4HUJZxg+KvpPAho4SBSpF/7CNfhLGnIB+f\nLBW7jdUa+dbOGRyOkVajj9x2EkIYDjwwVSz6oPONwc/BsvjSKBiCsbd62s2TqH6M\nEaspwR6pNmGD+VZHCi38q+i4WwKBgQDRTd7WlvQ/mVHP91kIh5rMyxo7BoCYfHP4\n1+pbOWo0mrMQ6Y/XWLAELVSzjVJ/W2jfpuP7QIyJPcWLocbVBwtgpucsE79Hhx6W\nDJFZv5bNw4U9pRBB3WQ1XMPXWhyTQBSsf/+xW4sWg6nh+6juDVWCVIIrzDNz/DDG\nF4ws7S7JlQKBgDl1/plxNdX1nQPiqIlOfWnpvOoWJhL5H4kyNOw9w2GGQNoroL+o\nkQnCNZldIMeJ7mdH0xJm8CwklABiu6NA1Pzb+COr/MoEVTdJVZuN5tluETDc7vBC\nIPPbNJOED5ILAkKHu9q3j/GeysApY8oIKHg85tB7sZmi4MUYYQLwXaBnAoGAFRGW\nndlCKui/EIUghDhKP9UuBcUIytC0R9dMMvSbmUH9RnWMUiwOQ81gWlaeeE12SBUf\nm9oKi4IZUY3uWuFDpu4GFSzDs2qJjKclNniPsJS+486qzUaA4NI+sH3UpIntytNz\npKHHZXPJdQMFJTlYYy8oj0oV39YB/t698IPLkuUCgYEAjubuLBtdJ1FelDJE1IkZ\nYOqr4hCBLmXfRv5+Pec2e9sE4wXVIdqfHxk6xpYdfX9olRYfSKTFFK1lcctKZvWU\nNX4IVuQyF6CLxUJD8cRCnp5YG/biW9vaXxTfWgb4P/g+8BznC9bJCWsVsewkMiRv\nn5C7CERGH1A9SjbUeyAAe4U=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-xc6fb@djangofcm-ffc7c.iam.gserviceaccount.com",
+  "client_id": "106217869141458747145",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-xc6fb%40djangofcm-ffc7c.iam.gserviceaccount.com"
 })
 
--- notify/views.py
+++ notify/views.py
@@ -4,10 +4,10 @@ from notify.api_exception import exceptions_error
 from notify.functions import to_one_user, to_all_users
 
 
-views = Blueprint("views", __name__, url_prefix="/notification")
+views = Blueprint("views", __name__)
 
 
-@views.post('/<userId>')
+@views.post('/notification/send/<userId>')
 def send_user(userId):
     profile = UserToken.query.filter_by(userId=userId).first()
     senderId = request.headers.get('senderId')
@@ -24,7 +24,7 @@ def send_user(userId):
 
 
 
-@views.post('/all')
+@views.post('/notification/send')
 def send_all():
     profiles = UserToken.query.all()
     senderId = request.headers.get('senderId')
@@ -41,10 +41,11 @@ def send_all():
 
 
 
-@views.post('/register')
+@views.post('/notification/register')
 def register_user():
     userId = request.headers.get('userId')
     token = request.get_json().get('token', '')
+    profile = UserToken.query.filter_by(userId=userId).first()
 
     if not userId:
         return exceptions_error("User id is empty", 400)
@@ -52,17 +53,19 @@ def register_user():
     if not token:
         return exceptions_error("Token is empty", 400)
 
-    if UserToken.query.filter_by(userId=userId).first():
-        return exceptions_error("User id already exists", 409)    
+    if not profile:
+        if UserToken.query.filter_by(token=token).first():
+            return exceptions_error("Token already exists", 409)
+        data = UserToken(userId=userId, token=token)
+        db.session.add(data)
+    elif profile:
+        profile.token = token
 
-    data = UserToken(userId=userId, token=token)
-    db.session.add(data)
     db.session.commit()
-
     return Response(), 200
 
 
-@views.get('/all')
+@views.get('/notification/tokens')
 def get_all():
     profiles = UserToken.query.all()
 
@@ -76,20 +79,20 @@ def get_all():
     
     return jsonify({'data': data}), 200
 
-@views.get('/<userId>')
-def get_notifications(userId):
+@views.get('/notification')
+def get_notifications():
+    userId = request.headers.get('userId')
     profile = Notification.query.filter_by(userId=userId).order_by().all()
     if profile:
 
         notifications = []
-        
+
         for item in profile:
             notifications.append({
                 'id': item.id,
                 'userId': item.userId,
                 'senderId': item.senderId,
                 'message': item.message,
-                'notificationType': "Specific",
                 'createdAt': item.createdAt
             })
         return jsonify({'notifications': notifications}), 200
